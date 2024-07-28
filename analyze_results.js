12adb0adee09130ff8c4f6a2f25527d9af7f79e6
leedyun <105830643+leedyun@users.noreply.github.com>
2024-07-29 02:38:00 +0900

    Update analyze_results.js

--- analyze_results.js
+++ analyze_results.js
@@ -19,13 +19,18 @@ async function analyzeCode() {
   const studentCode = fs.readFileSync('student_code.cpp', 'utf8');
   
   const prompt = `
-  다음은 학생에게 주어진 문제와 학생이 작성한 코드입니다. 코드를 리뷰하고 잘못된 부분을 지적하며, 어떻게 고치면 좋을지와 그 이유를 설명해 주세요. 특이점이 없다면 피드백을 안 해줘도 됩니다. 또한, 코드의 개선점을 제안해 주세요. 응답은 반드시 한국어로 해주세요.
+  다음은 학생에게 주어진 문제와 학생이 작성한 코드입니다. 코드를 리뷰하고 각 부분별로 잘못된 부분을 지적하며, 어떻게 고치면 좋을지와 그 이유를 설명해 주세요. 특이점이 없다면 피드백을 안 해줘도 됩니다. 또한, 코드의 개선점을 제안해 주세요. 응답은 반드시 한국어로 해주세요.
 
   [문제]
   ${problemStatement}
 
   [학생이 작성한 코드]
   ${studentCode}
+
+  리뷰 형식:
+  1. 문제점: 설명
+  2. 잘못된 코드: 
+  3. 올바른 코드: 
   `;
 
   const response = await openai.chat.completions.create({
